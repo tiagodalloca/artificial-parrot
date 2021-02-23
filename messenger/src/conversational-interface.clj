6c8e657303669b554a8d256ac9f989633e5a9d63
tiagodalloca <tdalloca@gmail.com>
2021-02-23 18:50:32 -0300

    changed naming

--- messenger/src/conversational-interface.clj
+++ messenger/src/conversational-interface.clj
@@ -2,27 +2,29 @@
 
 (defprotocol MessengerInterfaceProtocol
   "MessengerInterfaceProtocol"
-  (put-message [this message direction]
+  (put-message [this text direction]
     "Puts a new `messege` in the queue to be delivered. `direction` may be :incoming or :outgoing")
-  (deliver-messeges! [this]
+  (deliver-messeges! [this sent-messages-atom]
     "Deliver messeges in the queue"))
 
 (defrecord TerminalMessenger
     [queue]
   MessengerInterfaceProtocol
-  (put-message [this message direction]
-    (assoc this :queue (conj queue {:message message :direction direction})))
-  (deliver-messeges! [this]
-    (do (doseq [{:keys [message direction]} queue]
-          (println (str (if-not (= direction :outgoing) ">" "") message))
-          (assoc this :queue (clojure.lang.PersistentQueue/EMPTY))))))
+  (put-message [this text direction]
+    (assoc this :queue (conj queue {:text text :direction direction})))
+  (deliver-messeges! [this sent-messages-atom]
+    (do (doseq [{:keys [text direction] :as message} queue]
+          (println (str (if-not (= direction :outgoing) ">" "") text))
+          (swap! sent-messages-atom (fn [x] (conj x message))))
+        (assoc this :queue (clojure.lang.PersistentQueue/EMPTY)))))
 
 (comment
-  (-> (->TerminalMessenger (clojure.lang.PersistentQueue/EMPTY))
-      (put-message "salve" :outgoing)
-      (put-message "salve salve" :incoming)
-      (put-message "nossa! funcionou :)" :incoming)
-      (put-message (read-line) :outgoing)
-      (put-message "tendi nada kk" :incoming)
-      (deliver-messeges!)))
+  (let [sent-messages-atom (atom [])]
+    (-> (->TerminalMessenger (clojure.lang.PersistentQueue/EMPTY))
+        (put-message "salve" :outgoing)
+        (put-message "salve salve" :incoming)
+        (put-message "nossa! funcionou :)" :incoming)
+        (put-message "tendi nada kk" :incoming)
+        (deliver-messeges! sent-messages-atom))
+    @sent-messages-atom))
 
