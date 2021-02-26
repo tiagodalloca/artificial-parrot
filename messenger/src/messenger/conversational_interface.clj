1326f3ccefb126c3535b6c08a1c92b26d3a7dc7d
tiagodalloca <tdalloca@gmail.com>
2021-02-25 23:31:33 -0300

    terminal interface tested and user system configured

--- messenger/src/messenger/conversational_interface.clj
+++ messenger/src/messenger/conversational_interface.clj
@@ -3,34 +3,41 @@
 (defprotocol MessengerInterfaceProtocol
   "MessengerInterfaceProtocol"
   (put-message [this text direction]
-    "Puts a new `messege` in the queue to be delivered. `direction` may be :incoming or :outgoing")
-  (deliver-messeges! [this] [this sent-messages-atom]
-    "Deliver messeges in the queue"))
+    "Puts a new `message` in the queue to be delivered. `direction` may be :incoming or :outgoing")
+  (deliver-messages! [this] [this last-sent-message-atom]
+    "Deliver messages in the queue"))
 
 (defrecord TerminalMessenger
     [queue]
   MessengerInterfaceProtocol
   (put-message [this text direction]
     (assoc this :queue (conj queue {:text text :direction direction})))
-  (deliver-messeges! [this sent-messages-atom]
+  (deliver-messages! [this last-sent-message-atom]
     (do (doseq [{:keys [text direction] :as message} queue]
-          (when (= direction :incoming) (println (str ">" text)))
-          (when sent-messages-atom
-            (swap! sent-messages-atom
-                   (fn [x] (conj x (assoc message :timestamp
-                                         (str (java.time.Instant/now))))))))
+          (if (= direction :incoming)
+            (println (str ">" text))
+            (println text))
+          (when last-sent-message-atom
+            (reset! last-sent-message-atom
+                    (assoc message :timestamp (str (java.time.Instant/now))))))
         (assoc this :queue (clojure.lang.PersistentQueue/EMPTY))))
-  (deliver-messeges! [this]
-    (deliver-messeges! this nil)))
+  (deliver-messages! [this]
+    (deliver-messages! this nil)))
 
 (comment
-  (let [sent-messages-atom (atom [])]
+  (let [last-sent-message-atom (atom nil)]
     (-> (->TerminalMessenger (clojure.lang.PersistentQueue/EMPTY))
         (put-message "salve" :outgoing)
         (put-message "salve salve" :incoming)
         (put-message "nossa! funcionou :)" :incoming)
         (put-message "tendi nada kk" :incoming)
-        (deliver-messeges! sent-messages-atom))
-    @sent-messages-atom))
+        (deliver-messages! last-sent-message-atom))
+    @last-sent-message-atom))
 
 (defn terminal-messenger [] (->TerminalMessenger (clojure.lang.PersistentQueue/EMPTY)))
+
+(defn messenger-put-message! [messenger-ref text direction]
+  (swap! messenger-ref (fn [m] (put-message m text direction))))
+
+(defn messenger-deliver! [messenger-ref last-sent-message-atom]
+  (swap! messenger-ref (fn [m] (deliver-messages! m last-sent-message-atom))))
