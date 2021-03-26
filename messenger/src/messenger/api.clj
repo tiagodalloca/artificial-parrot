441dcf5d0e57438488ead4db3c0deedff3988771
tiagodalloca <tdalloca@gmail.com>
2021-03-26 13:03:10 -0300

    api: added missing options

--- messenger/src/messenger/api.clj
+++ messenger/src/messenger/api.clj
@@ -1,11 +1,11 @@
 (ns messenger.api
-  (:require [messenger.api.routes :refer [routes]]
+  (:require [messenger.api.routes :refer [routes options]]
             [reitit.ring :as ring]
             [ring.adapter.jetty :as jetty]))
 
 (defn app []
   (ring/ring-handler
-   (ring/router (routes))))
+   (ring/router (routes) options)))
 
 (defn start-server []
   (with-local-vars [instance-app (app)]
@@ -13,3 +13,6 @@
 
 (comment
   (defonce server (start-server)))
+
+(comment
+  (.stop server))
--- messenger/src/messenger/api/routes.clj
+++ messenger/src/messenger/api/routes.clj
@@ -25,4 +25,39 @@
             :handler (fn [{{{:keys [text]} :body} :parameters}]
                        {:status 200 :body "ok"})}}]])
 
-
+(def options
+  {;;:reitit.middleware/transform dev/print-request-diffs ;; pretty diffs
+   ;;:validate spec/validate ;; enable spec validation for route data
+   ;;:reitit.spec/wrap spell/closed ;; strict top-level validation
+   :exception pretty/exception
+   :data
+   {:coercion
+    (reitit.coercion.malli/create
+     {;; set of keys to include in error messages
+      :error-keys
+      #{:type :coercion :in :schema :value :errors :humanized :transformed}
+      ;; schema identity function (default: close all map schemas)
+      :compile mu/closed-schema
+      ;; strip-extra-keys (effects only predefined transformers)
+      :strip-extra-keys true
+      ;; add/set default values
+      :default-values true
+      ;; malli options
+      :options nil})
+    :muuntaja m/instance
+    :middleware [;; query-params & form-params
+                 parameters/parameters-middleware
+                 ;; content-negotiation
+                 muuntaja/format-negotiate-middleware
+                 ;; encoding response body
+                 muuntaja/format-response-middleware
+                 ;; exception handling
+                 exception/exception-middleware
+                 ;; decoding request body
+                 muuntaja/format-request-middleware
+                 ;; coercing response bodys
+                 coercion/coerce-response-middleware
+                 ;; coercing request parameters
+                 coercion/coerce-request-middleware
+                 ;; multipart
+                 multipart/multipart-middleware]}})
